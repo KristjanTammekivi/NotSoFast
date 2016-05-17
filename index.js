6cb463fe641e9ac570fc420c612156286267c167
Kristjan Tammekivi <kristjan@cleveron.eu>
2016-05-17 22:43:10 +0300

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Promise = require('bluebird');
+const SetBucket = require('./SetBucket');
+
+module.exports = function (opts) {
+
+	const bucket = new SetBucket();
+
+	return function () {
+
+		let timeout;
+
+		const ticket = new Promise(function (reject, resolve) {
+			timeout = setTimeout(reject, opts.timeout);
+			bucket.add();
+		});
+
+		queue.push
+
+	};
+
+};
